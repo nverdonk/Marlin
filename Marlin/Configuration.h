4cf074fe63a23d4f5d7d5ccb65c1db82779398c9
nverdonk <46147269+nverdonk@users.noreply.github.com>
2021-04-05 13:26:42 -0700

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1058,7 +1058,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET {-51.8, -21.3, -1.75}
+#define NOZZLE_TO_PROBE_OFFSET {-51.8, -21.3, -1.76}
 
 // M206 X-2.30 Y-12.00 Z0.00
 
